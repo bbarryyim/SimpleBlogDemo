24159c6f872f66828e0dbf194e5ea1c2e918877e
barryyim <bbarryyim@gmail.com>
2019-12-09 09:33:59 -0800

    Running docker container in detached mode

--- dockerRun.sh
+++ dockerRun.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
 CONTAINER_NAME=simple-blog-demo
-echo -"\nSetting docker container name as ${CONTAINER_NAME}\n"
+echo "****** Setting docker container name as ${CONTAINER_NAME} ******"
 
 IMAGE_NAME=${CONTAINER_NAME}:dev
-echo -"\nSet docker image name as ${IMAGE_NAME}\n"
+echo "****** Set docker image name as ${IMAGE_NAME} ******"
 PORT=8080
-echo -"Set docker image PORT to ${PORT}\n"
+echo "****** Set docker image PORT to ${PORT} ******"
 
-echo -"Create target jar...\n"
+echo "****** Create target jar******"
 mvn clean package
 
-echo -"\nStop running Docker containers with image tag ${CONTAINER_NAME}, and remove them...n"
+echo "****** Stop running Docker containers with image tag ${CONTAINER_NAME}, and remove them ******"
 docker stop $(docker ps -a | grep ${CONTAINER_NAME} | awk '{print $1}')
 docker rm $(docker ps -a | grep ${CONTAINER_NAME} | awk '{print $1}')
 
-echo -"\nDocker build image with name ${IMAGE_NAME}...\n"
+echo "****** Docker build image with name ${IMAGE_NAME} ******"
 docker build -t ${IMAGE_NAME} -f Docker/Dockerfile .
 
-echo -"\nStart Docker container of the image ${IMAGE_NAME} with name ${CONTAINER_NAME}...\n"
-docker run --rm -i -p ${PORT}:${PORT} \
+echo "****** Run Docker container of the image in detatched mode: ${IMAGE_NAME} with name ${CONTAINER_NAME} ******"
+docker run -d --rm -i -p ${PORT}:${PORT} \
     --name ${CONTAINER_NAME} \
     ${IMAGE_NAME}
